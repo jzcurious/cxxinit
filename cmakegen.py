f067ce4578d59b76b0d165c753ab5ddb9a4963db
jzcurious <paranoid.hpc.io@gmail.com>
2024-11-02 11:58:29 +0700

    add jinja2

---
+++ cmakegen.py
@@ -0,0 +1,3 @@
+class CmakeGen:
+    def __init__(self):
+        pass
--- dev.py
+++ dev.py
@@ -1,11 +1,17 @@
 from explorer import Explorer
+from structure import Structure
 from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
 
 if __name__ == "__main__":
     e = Explorer(Path("/home/void/Projects/cxxinit/cxxinit.yml"))
-    print(e.subdirs_paths)
     print(e.root_path)
-    print(e.project_name)
-    print(e.std_version)
-    print(e.cxx_flags)
-    print(e.subdirs)
+    s = Structure(e.root_path, *e.subdirs_paths)
+    s.apply()
+
+    env = Environment(
+        loader=FileSystemLoader("./assets/templates/cmake"),
+    )
+
+    template = env.get_template("main.txt")
