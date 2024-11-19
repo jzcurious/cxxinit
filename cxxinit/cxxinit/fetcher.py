8bbb987c45bd18c5c8597463283e9a4fcd2572ab
jzcurious <paranoid.hpc.io@gmail.com>
2024-11-19 18:55:08 +0700

    fix dist

--- cxxinit/cxxinit/fetcher.py
+++ cxxinit/cxxinit/fetcher.py
@@ -21,39 +21,6 @@ def find_content(name: str) -> tuple[str, None | Traversable]:
     return name, None
 
 
-# def fetch_all(project: Project) -> tuple[list[Path], list[Path]]:
-#     content = project.fetch
-
-#     found = []
-#     not_found = []
-
-#     for item in content:
-#         if isinstance(item, dict):
-#             src_name, src_path = find_content(list(item.values())[0])
-#             dst_path = project.path.joinpath(list(item.keys())[0])
-#         else:
-#             src_name, src_path = find_content(item)
-#             dst_path = project.path.joinpath(item)
-
-#         if not src_path:
-#             not_found.append(src_name)
-#             continue
-
-#         found.append(src_path)
-
-#         if isinstance(src_path, Path):
-#             if src_path.is_file():
-#                 copy_file(src_path, dst_path)
-#             else:
-#                 copytree(src_path, dst_path)
-#             continue
-
-#         if src_path.is_file():
-#             pass
-
-#     return found, not_found
-
-
 def fetch_all(project: Project) -> tuple[list[Path], list[Path]]:
     content = project.fetch
 
@@ -73,6 +40,7 @@ def fetch_all(project: Project) -> tuple[list[Path], list[Path]]:
             continue
 
         if isinstance(src_path, Path):
+            print("Path")
             if src_path.is_file():
                 copy_file(src_path, dst_path)
             else:
@@ -80,6 +48,7 @@ def fetch_all(project: Project) -> tuple[list[Path], list[Path]]:
             continue
 
         if isinstance(src_path, MultiplexedPath):
+            print("MultiplexedPath")
             if src_path.is_file():
                 with open(dst_path, "wb") as dst:
                     dst.write(src_path.read_bytes())
